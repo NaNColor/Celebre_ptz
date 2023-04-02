ec1e2ec696406ec711bb509cd7c73733edf1e0ee
roman <roman@roman.karelia.pro>
2023-04-02 20:11:39 +0300

    Adding a carousel + changing Galery

---
+++ mainpage/templatetags/my_filters.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def mod(value, arg):
+    try:
+        return int(value) % int(arg)
+    except (ValueError, ZeroDivisionError):
+        return 0
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from .models import Galery
+
 def index(request):
-    data = Galery.objects.all()
-    return render(request,'index.html',{'data': data})
+    buf_count = Galery.objects.count()
+    if buf_count % 3 == 0:
+        data = Galery.objects.all()
+    elif buf_count % 3 == 1:
+        buf_count -= 1
+        data = Galery.objects.filter(pk__lt=buf_count)
+    elif buf_count % 3 == 2:
+        buf_count -= 2
+        data = Galery.objects.filter(pk__lt=buf_count)
+
+    return render(request,'index.html',{'data': data, 'count': buf_count})
     #HttpResponse("MAIN")
     
 
