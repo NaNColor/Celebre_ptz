62d6d2489aeab81860eec7d2475bdbf0fea8d6b9
Rosamon <55803598+Rosamon@users.noreply.github.com>
2023-04-03 20:18:09 +0300

    Added links for carousel and some math

--- mainpage/views.py
+++ mainpage/views.py
@@ -7,11 +7,10 @@ def index(request):
     if buf_count % 3 == 0:
         data = Galery.objects.all()
     elif buf_count % 3 == 1:
-        buf_count -= 1
         data = Galery.objects.filter(pk__lt=buf_count)
     elif buf_count % 3 == 2:
-        buf_count -= 2
-        data = Galery.objects.filter(pk__lt=buf_count)
+        data = Galery.objects.filter(pk__lt=buf_count-1)
+    #data = Galery.objects.all()
 
     return render(request,'index.html',{'data': data, 'count': buf_count})
     #HttpResponse("MAIN")
