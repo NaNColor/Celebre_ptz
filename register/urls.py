8c95b262390414b6bb1933884c53479c8439695a
Rosamon <55803598+Rosamon@users.noreply.github.com>
2023-04-06 20:44:43 +0300

    added links

--- register/urls.py
+++ register/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
- path('register', views.register_start, name='register'),
+ path('', views.register_start, name='register'),
 ]
--- register/views.py
+++ register/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from .models import *
-
+#from mainpage.models import Galery
 def pageNotFound(request, exception):
-    return redirect('mainpage/index.html')
+    return redirect('')
     #HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 # получение данных из бд
 def register_start(request):
-    blocks = Blocks.objects.all()
+    #blocks = Blocks.objects.all()
     #сделать выборкупо времени, где исключаются все блоки с датой меньше завтрашнего дня
-    address = Address.object.all()
-    stylist = Stylist.object.all()
-    options = Option.object.all()
-    return render(request, "register.html", 
-    {"blocks": blocks, "address": address, "stylist": stylist, "options": options})
-    
-    
+    #address = Address.object.all()
+    #stylist = Stylist.object.all()
+    #options = Option.object.all()
+    #return render(request, "register.html",
+    #{"blocks": blocks, "address": address, "stylist": stylist, "options": options})
+    return render(request, "register.html")
 
 # Create your views here.
