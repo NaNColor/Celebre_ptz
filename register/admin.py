443f8aae7d8d7be62a1d535900a8c48056698cbf
Rosamon <55803598+Rosamon@users.noreply.github.com>
2023-04-17 20:00:21 +0300

    added times, schedule, blocks

--- register/admin.py
+++ register/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Appointment, Option, Stylist, Address
+from .models import Appointment, Option, Stylist, Address, Times, Blocks, WorkSchedule
 # Register your models here.
-admin.site.register(Appointment)
+class WorkScheduleAdmin(admin.ModelAdmin):
+    list_display = ('stylist', 'day_of_work', )
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'proof', )
+admin.site.register(Appointment, AppointmentAdmin)
 admin.site.register(Option)
 admin.site.register(Stylist)
 admin.site.register(Address)
+admin.site.register(Times)
+admin.site.register(WorkSchedule, WorkScheduleAdmin)
+admin.site.register(Blocks)
--- register/models.py
+++ register/models.py
@@ -8,7 +8,7 @@ from datetime import time
 def get_default_my_date():
     return "Запись от "+datetime.now().strftime("%d.%m.%Y (%H:%M)")
 class Appointment(models.Model):
-    title = models.CharField(max_length=255, default=get_default_my_date)
+    title = models.CharField(max_length=255, default=get_default_my_date, verbose_name = "Обозначение")
     client_name = models.CharField(max_length=255, verbose_name = "Имя")
     client_patronymic = models.CharField(max_length=255, default="", verbose_name = "Отчество")#otchestvo
     client_phone = models.CharField(max_length=13, verbose_name="Номер телефона")
@@ -20,7 +20,7 @@ class Appointment(models.Model):
     stylist = models.ForeignKey('Stylist',
         on_delete=models.SET_DEFAULT, default=None, verbose_name = "Стилист")#stylist_id
     address = models.ForeignKey('Address', on_delete=models.PROTECT, to_field='id', default=1, verbose_name = "Адрес")#address_id
-    proof = models.BooleanField(default=False)#podtverzdeno?
+    proof = models.BooleanField(default=False, verbose_name = "Подтверждено" )#podtverzdeno?
 
     def __str__(self):
         return self.title
@@ -49,49 +49,33 @@ class Address(models.Model):
 
     def __str__(self):
         return self.city+" "+self.street+" "+self.building
-# # DAY_CHOICES = (
-# #     (datetime.strptime("Mon"), "Понедельник"),
-# #     (datetime.strptime("Tue"), "Вторник"),
-# #     (datetime.strptime("Wen"), "Среда"),
-# #     (datetime.strptime("Thu"), "Четверг"),
-# #     (datetime.strptime("Fri"), "Пятница"),
-# #     (datetime.strptime("Sat"), "Суббота"),
-# #     (datetime.strptime("Sun"), "Воскресенье"),
-# # )
-# # TIME_CHOICES = (
-# #     (time.strptime("9:40", "%H:%M"), "9:40"),
-# #     (time.strptime("10:20", "%H:%M"), "10:20"),
-# #     (time.strptime("11:00", "%H:%M"), "11:00"),
-# #     (time.strptime("11:40", "%H:%M"), "11:40"),
-# #     (time.strptime("12:20", "%H:%M"), "12:20"),
-# #     (time.strptime("13:00", "%H:%M"), "13:00"),
-# #     (time.strptime("13:40", "%H:%M"), "13:40"),
-# #     (time.strptime("14:20", "%H:%M"), "14:20"),
-# #     (time.strptime("15:00", "%H:%M"), "15:00"),
-# #     (time.strptime("15:40", "%H:%M"), "15:40"),
-# #     (time.strptime("16:20", "%H:%M"), "16:20"),
-# #     (time.strptime("17:00", "%H:%M"), "17:00"),
-# #     (time.strptime("17:40", "%H:%M"), "17:40"),
-# #     (time.strptime("18:20", "%H:%M"), "18:20"),
-# #     (time.strptime("19:00", "%H:%M"), "19:00"),
-# #     (time.strptime("19:40", "%H:%M"), "19:40"),
-# # )
 
-# class Blocks(models.Model):
-#     number = models.IntegerField()#1-16 
-#     #time = models.ForeignKey('Times', on_delete=models.PROTECT)#time_id
-#     #occupied = models.BooleanField(default=False)#zanato?
-#     date = models.DateField()
-#     # day_name = DAY_CHOICES
-#     # time  = TIME_CHOICES
-#     appointment = models.ForeignKey('Appointment', on_delete=models.SET_DEFAULT, default = 'null', null = True)
-#     #занятость смотрим по null в appointmemt
-#     stylist = models.ForeignKey('Stylist', on_delete=models.PROTECT)
-#     address = models.ForeignKey('Address', on_delete=models.PROTECT, default = 0)
-    
-# # class Times(models.Model):
-#     # time_str = models.CharField()
-#     # time_clock = models.TimeField()
-    
-#     # def __str__():
-#         # return self.time_str
+class WorkSchedule(models.Model):
+    stylist = models.ForeignKey('Stylist',
+        on_delete = models.SET_DEFAULT, default=None, verbose_name = "Стилист")#stylist_id
+    day_of_work = models.DateField(verbose_name = "Рабочий день", null = True)
+    sick_day = models.BooleanField(default=False, verbose_name = "Больничный")
+    def __str__(self):
+        if self.day_of_work != 'null':
+            day = self.day_of_work.strftime('%d.%m.%Y')
+            return  day
+        else:
+            return  "123"
+
+class Blocks(models.Model):
+    #number = models.IntegerField()#1-16
+    time = models.ForeignKey('Times', on_delete=models.PROTECT)#time_id
+    #occupied = models.BooleanField(default=False)#zanato?
+    date = models.DateField() #now.strftime("%A") = day of week
+    # day_name = DAY_CHOICES
+    # time  = TIME_CHOICES
+    appointment = models.ForeignKey('Appointment', on_delete=models.SET_DEFAULT, default = 'null', null = True)
+    #занятость смотрим по null в appointmemt
+    stylist = models.ForeignKey('Stylist', on_delete=models.PROTECT)
+    address = models.ForeignKey('Address', on_delete=models.PROTECT, to_field='id', default=1)
+
+class Times(models.Model):
+    time_clock = models.TimeField(verbose_name ="Время")
+    def __str__(self):
+        time_view = self.time_clock.strftime("%H:%M")
+        return time_view
